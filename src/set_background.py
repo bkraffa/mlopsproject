4048f9ab014f3c8ed5f40dda0f41b93331e56ef5
bkraffa <brunocaraffa@gmail.com>
2023-04-11 23:33:01 -0300

    first commit

---
+++ src/set_background.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import base64
+
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_background(png_file):
+    bin_str = get_base64_of_bin_file(png_file) 
+    page_bg_img = '''
+    <style>
+    .stApp {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: scroll; # doesn't work
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
---
+++ streamlit.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from src.set_background import set_background
+
+st.markdown('<h1 style="color:white;">Resnet50 Image classification Model</h1>', unsafe_allow_html=True)
+
+set_background('content/bg.webp')
+
+
